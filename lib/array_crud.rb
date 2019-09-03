ae5d48b447c31314d9876fed61b12dff9545609e
Sean Eriksen <eriksensean@yahoo.com>
2019-09-03 16:36:06 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -15,31 +15,26 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  music = ["pop", "edm", "rock", "dance"]
-  dance = music.pop
+  
+  
 end
 
 def remove_element_from_start_of_array(array)
-  music = ["pop", "edm", "rock", "dance"]
-  pop = music.shift
+  
 end
 
 def retrieve_element_from_index(array, index_number)
-  music = ["pop", "edm", "rock", "dance"]
-  music[1]
+  
 end
 
 def retrieve_first_element_from_array(array)
-  music = ["pop", "edm", "rock", "dance"]
-  music[0]
+  
 end
 
 def retrieve_last_element_from_array(array)
-  music = ["pop", "edm", "rock", "dance"]
-  music[-1]
+  
 end
 
 def update_element_from_index(array, index_number, element)
-music = ["pop", "edm", "rock", "dance"]
-music[2] = "trance"
+
 end
