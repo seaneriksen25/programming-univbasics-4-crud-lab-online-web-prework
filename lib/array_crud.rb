80a77d8c38aa42a3f5de4be9cbeb7f7cd34074bc
Sean Eriksen <eriksensean@yahoo.com>
2019-09-03 15:26:17 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,10 +8,11 @@ end
 
 def add_element_to_end_of_array(array, element)
   music = ["pop", "edm", "rock", "dance"]
-  music<<
+  music<< "rap"
 end
 
 def add_element_to_start_of_array(array, element)
+  music = ["pop", "edm", "rock", "dance"]
   
 end
 
