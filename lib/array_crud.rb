4cec34a348b68a7ba11f8266f53c7caa9f2d554f
Sean Eriksen <eriksensean@yahoo.com>
2019-09-03 16:32:48 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -11,8 +11,7 @@ def add_element_to_end_of_array(array, element)
   end
 
 def add_element_to_start_of_array(array, element)
-  music = ["pop", "edm", "rock", "dance"]
-  music.unshift("rap")
+  .unshift(variable)
 end
 
 def remove_element_from_end_of_array(array)
