dbff46fa7ce26d39482a8be11f4e63eb4bbc3e1e
Sean Eriksen <eriksensean@yahoo.com>
2019-09-03 17:08:32 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,11 +7,11 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-   << "trance"
+   array << "trance"
   end
 
 def add_element_to_start_of_array(array, element)
-  .unshift("rap")
+  array.unshift("rap")
 end
 
 def remove_element_from_end_of_array(array)
