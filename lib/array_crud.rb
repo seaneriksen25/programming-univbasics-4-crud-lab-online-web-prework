dd2612e05671a34a64b9a2ef948e445b6c3546fc
Sean Eriksen <eriksensean@yahoo.com>
2019-09-03 16:26:49 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  = ["array", "elememnt"]
-  << "variable"
+ array = ["array", "elememnt"]
+ array << "variable"
   end
 
 def add_element_to_start_of_array(array, element)
